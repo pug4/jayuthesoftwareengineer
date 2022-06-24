3814a4848762ef3122527fcfd9f9990a02194ed1
Jayu Patel <65745207+pug4@users.noreply.github.com>
2022-06-23 18:50:34 -0700

    Add files via upload

---
+++ form.php
@@ -0,0 +1,9 @@
+<?php
+    $name = $_POST['fname'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+
+
+
+    echo $_POST['fname'];
+?>
--- index.js
+++ index.js
@@ -1,52 +1,6 @@
 import AOS from 'aos'
 
-AOS.init();
-const [red, green, blue] = [69, 111, 225]
-const colorChangeBackground = document.querySelector('.colorChangeBackground')
-
-window.addEventListener('scroll', () => {
-  const y = 1 + (window.scrollY || window.pageYOffset) / 150
-  const [r, g, b] = [red/(y), green/(y), blue/(y)].map(Math.round)
-  colorChangeBackground.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
-})
+// AOS.init();
+// window.addEventListener('load', AOS.refresh)
 
 
-// Initialising the canvas
-var canvas = document.querySelector('canvas'),
-    ctx = canvas.getContext('2d');
-
-// Setting the width and height of the canvas
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-// Setting up the letters
-var letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ';
-letters = letters.split('');
-
-// Setting up the columns
-var fontSize = 10,
-    columns = canvas.width / fontSize;
-
-// Setting up the drops
-var drops = [];
-for (var i = 0; i < columns; i++) {
-  drops[i] = 1;
-}
-
-// Setting up the draw function
-function draw() {
-  ctx.fillStyle = 'rgba(0, 0, 0, .1)';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  for (var i = 0; i < drops.length; i++) {
-    var text = letters[Math.floor(Math.random() * letters.length)];
-    ctx.fillStyle = '#0f0';
-    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
-    drops[i]++;
-    if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
-      drops[i] = 0;
-    }
-  }
-}
-
-// Loop the animation
-setInterval(draw, 40);
